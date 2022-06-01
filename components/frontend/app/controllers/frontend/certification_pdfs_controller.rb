a54be7fb536b987eb5294f394edcbccece8bef53
Ernest Lee <oolzpishere@126.com>
2022-06-01 12:05:37 +0800

    finish render pdf test

--- components/frontend/app/controllers/frontend/certification_pdfs_controller.rb
+++ components/frontend/app/controllers/frontend/certification_pdfs_controller.rb
@@ -1,7 +1,7 @@
 module Frontend
   class CertificationPdfsController < ApplicationController
     # before_action :set_certification_pdf, only: %i[ show edit update destroy ]
-
+    include ActionView::Helpers::AssetUrlHelper
     # GET /certification_pdfs
     def index
       @certification_pdfs = CertificationPdf.all
@@ -12,8 +12,26 @@ module Frontend
       respond_to do |format|
         format.html
         format.pdf do
-          pdf = Prawn::Document.new
-          pdf.text "Hello World"
+          # image = MiniMagick::Image.open("certs/nation.jpg")
+          assets_path = Rails.root.join('app', 'assets')
+          backgroud_image = assets_path.join('images', 'certs', 'nation.jpg')
+          pdf = Prawn::Document.new(page_size: [800, 594], 
+                                    page_layout: :portrait,
+                                    background:  backgroud_image)
+          # pdf.image(app.asset_path("ccerts/nation.jpgerts/nation.jpg"), width: pdf.bounds.width, height: pdf.bounds.height)
+
+          pdf.font("#{assets_path}/fonts/Songti.ttc", size: 16) do
+            pdf.stroke_axis
+            pdf.move_down 150 
+
+            pdf.draw_text("测试" + " " * 30 + "2022年5月30日" , at: [130,360])
+
+            pdf.move_down 25
+            pdf.draw_text "小学数学", :at => [100,325]
+            pdf.draw_text "小学数学", :at => [550,325]
+          end
+
+
           # send_data pdf.render_file,  filename: pdf.name, type: "application/pdf"
           # pdf.render_file 'assignment.pdf'
           send_data pdf.render,  filename: 'test.pdf', type: 'application/pdf', disposition: 'inline'
