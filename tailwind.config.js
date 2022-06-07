0a6d92bbadf751714bf88e2ad5e62a599632f8cf
Ernest Lee <oolzpishere@126.com>
2022-06-07 18:46:55 +0800

    draft style index and cert page

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
     }
   },
   plugins: [
-    // require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
     // require('@tailwindcss/aspect-ratio'),
     // require('@tailwindcss/typography'),
   ]
