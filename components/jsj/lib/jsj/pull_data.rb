74a1cc145f87786aa23d749ca49d3d4133c2acf2
Ernest Lee <oolzpishere@126.com>
2022-06-14 12:31:17 +0800

    add pull_latest2

--- components/jsj/lib/jsj/pull_data.rb
+++ components/jsj/lib/jsj/pull_data.rb
@@ -43,6 +43,10 @@ module Jsj
       PullForm.new(form_identify).pull_form
     end
 
+    def self.pull_latest(form_identify)
+      PullForm.new(form_identify).pull_latest
+    end
+
     def self.backup_db
       # Jsj::Backup.backup_db
     end
