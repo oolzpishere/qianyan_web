9f1c68665d347a098235ed48a423e0ba3826e4e3
Ernest Lee <oolzpishere@126.com>
2022-06-18 18:09:10 +0800

    update render pdfs

--- components/jsj/lib/jsj/render_pdfs.rb
+++ components/jsj/lib/jsj/render_pdfs.rb
@@ -2,20 +2,22 @@ module Jsj
   class RenderPdfs
     RENDER_LOCATION = "/Users/leeli/Downloads/pdfs"
 
-    def jsj_form_render(form_identify, filter: {})
-      attend_data = get_data(form_identify, filter: filter)
-      attend_data.each do |attend_datum|
-        render_pdf(attend_datum)
-      end
-      # render each datum
+    attr_reader :attend_form_data
+    def initialize(form_identify)
+      @attend_form_data = get_form_data(form_identify)
     end
 
-    def get_data(form_identify, filter: {})
+    def jsj_form_render( filter: {})
+      filter_attend_data(attend_form_data, filter: filter).each do |attend_datum|
+        render_pdf(attend_datum)
+      end
+    end
+
+    def get_form_data(form_identify)
       # where select
       jsj_form = Backend::JsjForm.find_by(form_identify: form_identify)
-      params = {jsj_form_id: jsj_form.id} 
-      params.merge!(filter)
-      attend_data = Backend::AttendDatum.includes(:jsj_form).where( params )
+      attend_data = Backend::AttendDatum.includes(:jsj_form).where( {jsj_form_id: jsj_form.id} )
+      # filter(attend_data)
     end
 
     def render_pdf(attend_datum)
@@ -55,7 +57,19 @@ module Jsj
         pdf.draw_text cert_parser.conference.sms_conf_name, :at => [904,535]
         pdf
       end
+      
+      def filter_attend_data(attend_data, filter: {})
+        return attend_data if filter.blank?
+        attend_data.select do |attend_datum|
+          match_filter?(attend_datum, filter)
+        end
+      end
 
+      def match_filter?(attend_datum, filter)
+        filter.each do |key, value|
+          return true if attend_datum.send(key).match(value)
+        end
+      end
 
   end
 end
