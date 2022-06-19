1e2bc1d12a2783146d93b4f4f72e00c6cb5614ba
Ernest Lee <oolzpishere@126.com>
2022-06-19 18:51:31 +0800

    update jsj_form

--- components/jsj/lib/jsj/render_pdfs/jsj_form.rb
+++ components/jsj/lib/jsj/render_pdfs/jsj_form.rb
@@ -24,38 +24,19 @@ module Jsj
         cert_parser = Frontend::CertParser.new(attend_datum)
         pdf = gen_country_pdf( attend_datum, cert_parser )
         file_name = [attend_datum.school, attend_datum.name].compact.join('-')
-        pdf.render_file "#{RenderPdfs::RENDER_LOCATION}/#{file_name}.pdf"
-
+        pdf.render_file "#{Jsj::RenderPdfs.render_location}/#{file_name}.pdf"
       end
 
       private
 
         def gen_country_pdf(attend_datum, cert_parser)
-          # image = MiniMagick::Image.open("certs/nation.jpg")
-          assets_path = Rails.root.join('app', 'assets')
-          public_path = Rails.root.join('public')
-          backgroud_image = assets_path.join('images', 'certs', "#{cert_parser.cert_bg_type}.jpg")
-          pdf = Prawn::Document.new(page_size: [1280, 950], 
-                                    page_layout: :portrait,
-                                    background:  backgroud_image)
-          # pdf.image(app.asset_path("ccerts/nation.jpgerts/nation.jpg"), width: pdf.bounds.width, height: pdf.bounds.height)
-          pdf.font_families.update( 
-            'songti' => {
-              normal: {file: "#{public_path}/fonts/FZ30.TTF"}
-            },
-            'msyh' => {
-            normal: { file: "#{public_path}/fonts/msyh.ttf" }
-          })
-          pdf.fallback_fonts(['msyh'])
-          pdf.font("songti", size: 26)
+          params = {
+            name: attend_datum.name,
+            date_range_str: cert_parser.date_range_str,
+            subject_name: cert_parser.conference.sms_conf_name
+          }
 
-            # pdf.stroke_axis
-          pdf.text_box(attend_datum.name, at: [270,620] )
-          pdf.draw_text(cert_parser.date_range_str, at: [595,595])
-          # pdf.move_down 25
-          pdf.draw_text cert_parser.conference.sms_conf_name, :at => [175,535]
-          pdf.draw_text cert_parser.conference.sms_conf_name, :at => [904,535]
-          pdf
+          Jsj::RenderPdfs::Render.new.render_country_pdf(params)
         end
         
         def filter_attend_data(attend_data, filter: {})
