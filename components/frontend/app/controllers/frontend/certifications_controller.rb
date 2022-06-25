c0e5352738a894de6469dbbe6a640e3bb9f54987
Ernest Lee <oolzpishere@126.com>
2022-06-25 21:47:48 +0800

    update certifications student pdf

--- components/frontend/app/controllers/frontend/certifications_controller.rb
+++ components/frontend/app/controllers/frontend/certifications_controller.rb
@@ -112,27 +112,11 @@ module Frontend
       end
 
       def gen_student_pdf(attend_datum, cert_parser)
-        # image = MiniMagick::Image.open("certs/nation.jpg")
-        assets_path = Rails.root.join('app', 'assets')
-        public_path = Rails.root.join('public')
-        # byebug
-        backgroud_image = assets_path.join('images', 'certs', "#{cert_parser.cert_bg_type}.jpg")
-        pdf = Prawn::Document.new(page_size: [1280, 950], 
-                                  page_layout: :portrait,
-                                  background:  backgroud_image)
-        # pdf.image(app.asset_path("ccerts/nation.jpgerts/nation.jpg"), width: pdf.bounds.width, height: pdf.bounds.height)
-
-        pdf.font("#{public_path}/fonts/FZ30.TTF", size: 26) do
-          # pdf.stroke_axis
-
-          pdf.draw_text(attend_datum.name, at: [270,600])
-          pdf.draw_text(cert_parser.date_range_str, at: [595,595])
-
-          # pdf.move_down 25
-          pdf.draw_text cert_parser.conference.sms_conf_name, :at => [175,535]
-          pdf.draw_text cert_parser.conference.sms_conf_name, :at => [904,535]
-        end
-        pdf
+        Jsj::RenderPdfs::Render.new.render_country_pdf({
+          name: attend_datum.name,
+          date_range_str: cert_parser.date_range_str,
+          subject_name: cert_parser.conference.sms_conf_name
+        })
       end
 
   end
